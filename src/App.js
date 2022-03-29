284fa8531fe28b45b8bf5ed06c93ba4010c8f9f9
Matteo Grilla <mgrilla@reach.me>
2022-03-29 14:33:05 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Note from './note';
+import eighth from './eighth.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const key = {
+  "0": "C",
+  "1": "C#",
+  "2": "D",
+  "3": "D#",
+  "4": "E",
+  "5": "F",
+  "6": "F#",
+  "7": "G",
+  "8": "G#",
+  "9": "A",
+  "t": "Bb",
+  "T": "Bb",
+  "e": "B",
+  "E": "B"
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {number: []};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    //this.setState({number: event.target.value});
+    let numStr = event.target.value;
+    let numStrArr = numStr.split('');
+    this.setState({number: numStrArr})
+  }
+
+  render(){
+    console.log(this.state);
+    return (
+      <div>
+        <h1><img src={eighth} className='eighth'/>Note Translator</h1>
+        <div className='input-container'>
+          <input type='text' id='number-input' placeholder='Enter your #...' onChange={this.handleChange} />
+        </div>
+        <div className='note-container'>
+          <ul>
+            {
+              ((this.state.number != [] && this.state.number != undefined) ?
+                this.state.number.map((n) => {
+                  if (n in [0,1,2,3,4,5,6,7,8,9] || ["t", "T", "e", "E"].includes(n)) {
+                    return <Note noteType={key[n]}/>
+                  }
+                })
+              : '')
+
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/note.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import note from './quarter.png';
+import './note.css';
+
+export default class Note extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {color: ''};  
+    }
+
+    componentDidMount(){
+        switch(this.props.noteType) {
+            case 'C':
+                // color='#a8e6cf';
+                this.setState({color: '#a8e6cf'});
+                break;
+            case 'C#':
+                // color='#dcedc1';
+                this.setState({color: '#dcedc1'});
+                break;
+            case 'D':
+                // color='#ffd3b6';
+                this.setState({color: '#ffd3b6'});
+                break;
+            case 'D#':
+                // color='#ffaaa5';
+                this.setState({color: '#ffaaa5'});
+                break;
+            case 'E':
+                // color='#ff8b94';
+                this.setState({color: '#ff8b94'});
+                break;
+            case 'F':
+                // color='#d2e0f2';
+                this.setState({color: '#d2e0f2'});
+                break;
+            case 'F#':
+                // color='#e3fffb';
+                this.setState({color: '#e3fffb'});
+                break;
+            case 'G':
+                // color='#d6ffd9';
+                this.setState({color: '#d6ffd9'});
+                break;
+            case 'G#':
+                // color='#fbece9';
+                this.setState({color: '#fbece9'});
+                break;
+            case 'A':
+                // color='#feffe1';
+                this.setState({color: '#feffe1'});
+                break;
+            case 'Bb':
+                // color='#afdfdb';
+                this.setState({color: '#afdfdb'});
+                break;
+            case 'B':
+                // color='#75c9cc';
+                this.setState({color: '#75c9cc'});
+                break;
+        }
+    }
+
+    render() {
+        return(
+            <li className='note' style={(this.state.color!=undefined)?{ borderColor: this.state.color }:''}>
+                <img src={note}/>
+                <p style={(this.state.color!=undefined)?{ color: this.state.color }:''}>{(this.props.noteType != undefined) ? this.props.noteType : ''}</p>
+            </li>
+        );
+    }
+}
